c65a2c2e2773cc79773c2b47d8bdcb4769b2133d
tori <aktoriam@students.cs.ubc.ca>
2019-11-08 18:34:12 -0800

    the sql file is updated

---
+++ sql/project3update.sql
@@ -0,0 +1,98 @@
+drop table Reservation cascade constraints purge
+/
+drop table Rent cascade constraints purge
+/
+drop table Vehicle cascade constraints purge
+/
+drop table VehicleType cascade constraints purge
+/
+drop table Customer cascade constraints purge
+/
+drop table Return cascade constraints purge
+/
+create table Reservation(
+confNo char(8) primary key,
+vtname varchar(8),
+cellphone char(10),
+fromDateTime date,
+toDateTime date)
+/
+create table Rent(
+rid char(8) primary key,
+vid char(8),
+cellphone char(10),
+fromDateTime date,
+toDateTime date,
+odometer varchar(9),
+cardName varchar(50),
+cardNo char(16),
+expDate date,
+confNo char(8))
+/
+create table Vehicle(
+vid char(8) primary key,
+vlicense char(6),
+make varchar(30),
+model varchar(30),
+year varchar(4),
+color varchar(20),
+odometer varchar(9),
+status varchar(20),
+vtname varchar(8),
+location varchar(20),
+city varchar(20))
+/
+create table VehicleType(
+vtname varchar(8) primary key,
+features varchar(100),
+wrate binary_float,
+drate binary_float,
+hrate binary_float,
+wirate binary_float,
+dirate binary_float,
+hirate binary_float,
+krate binary_float)
+/
+create table Customer(
+cellphone char(10) primary key,
+name varchar(50),
+address varchar(50),
+dlicense char(9))
+/
+create table Return(
+rid char(8) primary key,
+returnDateTime timestamp,
+odometer varchar(9),
+fulltank char(1),
+varlue integer)
+/
+alter table Reservation
+add foreign key(vtname)
+references VehicleType(vtname)
+/
+alter table
+Rent
+add foreign key (vid)
+references Vehicle (vid)
+/
+alter table Rent
+add foreign key (cellphone)
+references Customer (cellphone)
+/
+alter table Rent
+add foreign key (confNo)
+references Reservation (confNo)
+/
+alter table Vehicle
+add foreign key (vtname)
+references VehicleType (vtname)
+/
+create table Branch(
+location varchar(20),
+city varchar(20),
+primary key (location, city))
+/
+alter table Vehicle
+add foreign key (location, city)
+references Branch (location, city)
+/

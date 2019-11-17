0613b07f3f0df9a7ed558c41194a168a0231262d
john <likisongproduct@gmail.com>
2019-11-16 19:08:39 -0800

    created better inputs for customer and rent/reserve functions, wrote base body json for rest request

--- main.js
+++ main.js
@@ -1,63 +1,193 @@
 $( document ).ready(function()
 {
     console.log( "ready!" );
+    $("#make-reservation-fields").hide(); 
+    $("#make-return-fields").hide();
+
+    // hides "reservation" inputs when you click "view-vehicles"
+    $("#view-vehicles").click(function() {
+        $("#make-reservation-fields").hide(); 
+        $("#view-vehicles-fields").show(); 
+    })
+
+    // hides "view-vehicle" inputs when you click "reservation"
+    $("#make-reservation").click(function() {
+        $("#view-vehicles-fields").hide(); 
+        $("#make-reservation-fields").show(); 
+    })
+
+    $("#rent").click(function() {
+        $("make-return-fields").hide(); 
+        $("#make-rental-fields").show(); 
+    })
+
+    $("#return").click(function() {
+        $("#make-rental-fields").hide(); 
+        $("#make-return-fields").show(); 
+    })
+
+    $("#customer-button").click(function() {
+        let body;
+        let viewVehiclesSelected = $("#view-vehicles").is(':checked');
+        if (viewVehiclesSelected) {
+            body = viewVehiclesBody()
+        }
+        else {
+            body = reservationBody()
+        }
+        axios.post("/someUrl", body).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        })
+    });
+
+    $("#rent-reserve-button").click(function() {
+        console.log("rent-reserve");
+        let body;
+        let rentVehicleSelected = $("#rent").is(':checked');
+        if (rentVehicleSelected) {
+            body = rentBody();
+        }
+        else {
+            body = returnBody();
+        }      
+        axios.post("/someUrl", body).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        })  
+    });
+    
+
 });
 
-function customerButton() {
-    let viewVehiclesSelected = $("#view-vehicles").is(':checked');
+function returnBody() {
     let body = {};
-    // add some stuff to body to send
-    if (viewVehiclesSelected) {
-        axios.post("/someUrl", body).then((response) => {
-            console.log(response)
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-    else {
-        axios.post("/someUrl", body).then((response) => {
-            console.log(response)
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-    console.log(viewVehiclesSelected)
+    let rid = $("#return-rid").val();
+    let vtname = $("#return-vtname").val();
+    let city = $("#return-city").val();
+    let time = $("#return-time").val();
+    body["rid"] = rid;
+    body["vtname"] = vtname;
+    body["city"] = city;
+    body["time"] = time;
+    return body;
 }
 
-function clerkButton() {
-    let rentingVehiclesSelected = $("#rent").is(':checked');
+function rentBody() {
     let body = {};
-    // add some stuff to body to send
-    if (rentingVehiclesSelected) {
-        axios.post("/someUrl", body).then((response) => {
-            console.log(response)
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-    else {
-        axios.post("/someUrl", body).then((response) => {
-            console.log(response)
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-    console.log(rentingVehiclesSelected);
+    let phoneNum = $("#rent-phone-num").val();
+    let name = $("#rent-phone-num").val();
+    let address = $("#rent-phone-num").val();
+    let license = $("#rent-phone-num").val();
+    let vtname = $("#rent-phone-num").val();
+    let city = $("#rent-phone-num").val();
+    let from = $("#rent-phone-num").val();
+    let to = $("#rent-phone-num").val();
+    body["phoneNum"] = phoneNum;
+    body["name"] = name;
+    body["address"] = address;
+    body["city"] = city;
+    body["license"] = license;
+    body["vtname"] = vtname;
+    body["from"] = from;
+    body["to"] = to;
+    return body;
 }
 
-function generateButton() {
+function viewVehiclesBody() {
     let body = {};
-    // add some stuff to body to send
-    let dailyReturnsSelected = $("#daily-returns").is(':checked');
-    if (dailyReturnsSelected) {
-        axios.post("/someUrl", body).then((response) => {
-            console.log(response)
-        })
-    }
-    else {
-        axios.post("/someUrl", body).then((response) => {
-            console.log(response)
-        })
-    }
-    console.log(dailyReturnsSelected)
+    let carType = $("#vv-car-type").val();
+    let location = $("#vv-location").val();
+    let city = $("#vv-city").val();
+    let from = $("#vv-from").val();
+    let to = $("#vv-to").val();
+    body["carType"] = carType;
+    body["location"] = location;
+    body["city"] = city;
+    body["from"] = from;
+    body["to"] = to;
+    console.log(body);
+    return body;
 }
+
+function reservationBody() {
+    let body = {};
+    let phoneNum = $("#mr-phone-num").val();
+    let name = $("#mr-name").val();
+    let address = $("#mr-address").val();
+    let city = $("#mr-city").val();
+    let license = $("#mr-driver-license").val();
+    let vtname = $("#mr-vtname").val();
+    let from = $("#mr-from").val();
+    let to = $("#mr-to").val();
+    body["phoneNum"] = phoneNum;
+    body["name"] = name;
+    body["address"] = address;
+    body["city"] = city;
+    body["license"] = license;
+    body["vtname"] = vtname;
+    body["from"] = from;
+    body["to"] = to;
+    return body;
+} 
+
+// function customerButton() {
+//     let viewVehiclesSelected = $("#view-vehicles").is(':checked');
+//     let body = {};
+//     // add some stuff to body to send
+//     if (viewVehiclesSelected) {
+//         axios.post("/someUrl", body).then((response) => {
+//             console.log(response)
+//         }).catch((error) => {
+//             console.log(error)
+//         })
+//     }
+//     else {
+//         axios.post("/someUrl", body).then((response) => {
+//             console.log(response)
+//         }).catch((error) => {
+//             console.log(error)
+//         })
+//     }
+//     console.log(viewVehiclesSelected)
+// }
+
+// function clerkButton() {
+//     let rentingVehiclesSelected = $("#rent").is(':checked');
+//     let body = {};
+//     // add some stuff to body to send
+//     if (rentingVehiclesSelected) {
+//         axios.post("/someUrl", body).then((response) => {
+//             console.log(response)
+//         }).catch((error) => {
+//             console.log(error)
+//         })
+//     }
+//     else {
+//         axios.post("/someUrl", body).then((response) => {
+//             console.log(response)
+//         }).catch((error) => {
+//             console.log(error)
+//         })
+//     }
+//     console.log(rentingVehiclesSelected);
+// }
+
+// function generateButton() {
+//     let body = {};
+//     // add some stuff to body to send
+//     let dailyReturnsSelected = $("#daily-returns").is(':checked');
+//     if (dailyReturnsSelected) {
+//         axios.post("/someUrl", body).then((response) => {
+//             console.log(response)
+//         })
+//     }
+//     else {
+//         axios.post("/someUrl", body).then((response) => {
+//             console.log(response)
+//         })
+//     }
+//     console.log(dailyReturnsSelected)
+// }

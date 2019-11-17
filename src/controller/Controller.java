254535e2179c472700837c1b0ae0a3dc0521a840
james ens <james.ens@ubc365.onmicrosoft.com>
2019-11-16 17:50:21 -0800

    Moved database functions to data base handler
    TODO:
    Test current tunnelling setup
    Test running from cmd

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,89 +1,43 @@
 package controller;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import database.DatabaseConnectionHandler;
+import model.VehicleModel;
 
+import java.io.IOException;
 
 //This is the main controller class that will orchestrate everything.
-public class Controller {
-    private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
-    private static final String EXCEPTION_TAG = "[EXCEPTION]";
-    private static final String WARNING_TAG = "[WARNING]";
-    private Connection connection = null;
+public class Controller  {
+    private static DatabaseConnectionHandler dbHandler = null;
 
-    private HttpURLConnection http;
-    private String url = "index.html";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Controller c = new Controller();
         c.start();
+
 
     }
 
     //opens browser instance, navigates to web page, and logs in
     private void start() {
-        File htmlFile = new File(url);
+       /* File htmlFile = new File(url);
         try {
             Desktop.getDesktop().browse(htmlFile.toURI());
         } catch (Exception e) {
             System.out.printf(e.getMessage());
-        }
+        }*/
 
-        OpenHTTPConn();
 
-        login("ora_jamesens", "a98263510");
-       try {
-           viewVehicles();
-       }
-       catch (Exception e){
+        //login("ora_aktoriam@stu", "a42603381");
+        boolean didConnect = dbHandler.login("ora_jamesens", "a98263510");
 
-       }
+
+
+
     }
 
-    private void OpenHTTPConn() {
-        try {
-            URL url = new URL("http://127.0.0.1:8080/index.html");
-            URLConnection con = url.openConnection();
-            http = (HttpURLConnection) con;
-        } catch (Exception e) {
-            System.out.printf(e.getMessage());
-        }
+    private void OpenHTTPConn() throws IOException {
 
-        try {
-            http.setRequestMethod("POST"); // PUT is another valid option
-            http.setDoOutput(true);
-        } catch (Exception e) {
-            System.out.printf(e.getMessage());
-        }
-    }
-
-    // connects to Oracle database with supplied username and password
-
-    public boolean login(String username, String password) {
-        try {
-            if (connection != null) {
-                connection.close();
-            }
-
-            connection = DriverManager.getConnection(ORACLE_URL, username, password);
-            connection.setAutoCommit(false);
-
-            System.out.println("\nConnected to Oracle!");
-            return true;
-        } catch (SQLException e) {
-            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
-            return false;
-        }
     }
 
     //TODO
@@ -92,19 +46,8 @@ public class Controller {
     }
 
     //TODO
-    public void viewVehicles() throws IOException {
-
-
-        byte[] out = "{\"username\":\"root\",\"password\":\"password\"}".getBytes(StandardCharsets.UTF_8);
-        int length = out.length;
-        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        http.connect();
-
-        try (OutputStream os = http.getOutputStream()) {
-            os.write(out);
-        } catch (Exception e) {
-            System.out.printf(e.getMessage());
-        }
+    public static VehicleModel[] viewVehicles()  {
+        return dbHandler.getVehicleInfo();
     }
 
     //TODO
@@ -126,6 +69,8 @@ public class Controller {
     public void generateReport() {
 
     }
+
+
 }
 
 

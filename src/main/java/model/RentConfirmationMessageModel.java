f6122921f71af54a95b69f11122c4a69e107554e
tori <aktoriam@students.cs.ubc.ca>
2019-11-20 14:57:14 -0800

    RentConfirmationMessageModel has been modified.

--- src/main/java/model/RentConfirmationMessageModel.java
+++ src/main/java/model/RentConfirmationMessageModel.java
@@ -5,47 +5,34 @@ import java.util.Date;
 
 public class RentConfirmationMessageModel {
     private final int rid;
-    private final int vid;
     private final String cellphone;
     private final Timestamp fromDateTime;
     private final Timestamp toDateTime;
-    private final int odometer;
     private final String cardName;
     private final String cardNo;
     private final Date expDate;
+    private final int odometer;
     private final int confNo;
-
-    private final int vlicense;
-    private final String make;
-    private final String model;
-    private final String year;
-    private final String color;
-    private final String status;
+    private final int vid;
+    private final String vlicense;
     private final String vtname;
     private final String location;
     private final String city;
 
-
-    public RentConfirmationMessageModel(int rid, int vid, String cellphone, Timestamp fromDateTime,
-                                        Timestamp toDateTime, int odometer, String cardName, String cardNo, Date expDate,
-                                        int confNo, int vlicense, String make, String model, String year, String color,
-                                        String status, String vtname, String location, String city) {
+    public RentConfirmationMessageModel(int rid, String cellphone, Timestamp fromDateTime, Timestamp toDateTime,
+                                        String cardName, String cardNo, Date expDate, int odometer, int confNo, int vid,
+                                        String vlicense, String vtname, String location, String city) {
         this.rid = rid;
-        this.vid = vid;
         this.cellphone = cellphone;
         this.fromDateTime = fromDateTime;
         this.toDateTime = toDateTime;
-        this.odometer = odometer;
         this.cardName = cardName;
         this.cardNo = cardNo;
         this.expDate = expDate;
+        this.odometer = odometer;
         this.confNo = confNo;
+        this.vid = vid;
         this.vlicense = vlicense;
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.status = status;
         this.vtname = vtname;
         this.location = location;
         this.city = city;
@@ -53,10 +40,6 @@ public class RentConfirmationMessageModel {
 
     public int getRid() {
         return rid;
-    }
-
-    public int getVid() {
-        return vid;
     }
 
     public String getCellphone() {
@@ -71,10 +54,6 @@ public class RentConfirmationMessageModel {
         return toDateTime;
     }
 
-    public int getOdometer() {
-        return odometer;
-    }
-
     public String getCardName() {
         return cardName;
     }
@@ -87,32 +66,20 @@ public class RentConfirmationMessageModel {
         return expDate;
     }
 
+    public int getOdometer() {
+        return odometer;
+    }
+
     public int getConfNo() {
         return confNo;
     }
 
-    public int getVlicense() {
+    public int getVid() {
+        return vid;
+    }
+
+    public String getVlicense() {
         return vlicense;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getStatus() {
-        return status;
     }
 
     public String getVtname() {

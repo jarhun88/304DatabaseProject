afac440bb8900ef9e1fe5bff6d7174ff05336635
john <likisongproduct@gmail.com>
2019-11-20 21:49:45 -0800

    cors request now works for http://127.0.0.1:8081/

--- src/main/java/api/GreetingController.java
+++ src/main/java/api/GreetingController.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 @RestController
 public class GreetingController {
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/view-vehicles", consumes = "multipart/form-data", produces = "application/json")
     public String viewVehicles(@RequestPart(name="carType", required=false) String carType,
                            @RequestPart(name="location", required=false) String location,
@@ -20,12 +20,12 @@ public class GreetingController {
                            @RequestPart(name="from", required=false) String from,
                            @RequestPart(name="to", required=false) String to) {
 
-        JSONArray mJSONArray = new JSONArray(Arrays.asList(Controller.viewVehicles(carType, location, city, from, to)));
-        return mJSONArray.toString();
-//        return "view-vehicles";
+//        JSONArray mJSONArray = new JSONArray(Arrays.asList(Controller.viewVehicles(carType, location, city, from, to)));
+//        return mJSONArray.toString();
+        return "view-vehicles";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/reservation", consumes = "multipart/form-data", produces = "application/json")
     public String reservation(@RequestPart(name="phoneNum", required=false) String phoneNum,
                               @RequestPart(name="name", required=false) String name,
@@ -38,7 +38,7 @@ public class GreetingController {
         return "reservation";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/rent", consumes = "multipart/form-data", produces = "application/json")
     public String rent(@RequestPart(name="phoneNum", required=false) String phoneNum,
                        @RequestPart(name="name", required=false) String name,
@@ -51,7 +51,7 @@ public class GreetingController {
         return "rent";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/return", consumes = "multipart/form-data", produces = "application/json")
     public String returns(@RequestPart(name="rid", required=false) String rid,
                           @RequestPart(name="vtname", required=false) String vtname,
@@ -60,19 +60,19 @@ public class GreetingController {
         return "return";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/daily-returns", consumes = "multipart/form-data", produces = "application/json")
     public String dailyReturns(@RequestPart(name="date", required=false) String date) {
         return "dailyreturns";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/daily-rentals", consumes = "multipart/form-data", produces = "application/json")
     public String dailyRentals(@RequestPart(name="date", required=false) String date) {
         return "dailyrentals";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/daily-returns-branch", consumes = "multipart/form-data", produces = "application/json")
     public String dailyReturnsBranch(@RequestPart(name="date", required=false) String date,
                                      @RequestPart(name="address", required=false) String address,
@@ -80,7 +80,7 @@ public class GreetingController {
         return "dailyReturnsBranch";
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
+    @CrossOrigin(origins = {"http://localhost:8081", "http://127.0.0.1:8081", "http://206.87.116.219:8081"})
     @PostMapping(path = "/daily-rentals-branch", consumes = "multipart/form-data", produces = "application/json")
     public String dailyRentalsBranch(@RequestPart(name="date", required=false) String date,
                                      @RequestPart(name="address", required=false) String address,
